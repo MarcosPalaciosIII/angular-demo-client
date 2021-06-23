784c2aa8d3b769766b13d9d769a5a44b296100e8
Marcos Palacios <MPLI@C02WV0P2G8WN.local>
2021-06-22 20:38:39 -0400

    initial commit

---
+++ apps/demo-client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-client-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo-client';
+}
